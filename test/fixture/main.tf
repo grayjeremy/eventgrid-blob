591081f30c1564849bba080fecd6c65302601f9d
Jeremy Gray <jegra@microsoft.com>
2019-06-04 10:40:29 -0400

    text fixture formatting

--- test/fixture/main.tf
+++ test/fixture/main.tf
@@ -1,10 +1,10 @@
 module "eventgrid-blob" {
   source = "../../"
 
-  resourceGroupName = "terraform-event-grid-test"
-  defaultLocation = "eastus"
+  resourceGroupName  = "terraform-event-grid-test"
+  defaultLocation    = "eastus"
   storageAccountName = "yomrdj132428test"
-  siteName = "jeremysweettestsite45678"
-  hostingPlanName = "viewerhost"
-  armTemplateUrl = "https://raw.githubusercontent.com/grayjeremy/azure-event-grid-viewer/add-output-variable/azuredeploy.json"
+  siteName           = "jeremysweettestsite45678"
+  hostingPlanName    = "viewerhost"
+  armTemplateUrl     = "https://raw.githubusercontent.com/grayjeremy/azure-event-grid-viewer/add-output-variable/azuredeploy.json"
 }
